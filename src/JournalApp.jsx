c0a85ba5b087e95eebe90f7cc22d31758f4d30e6
Rasec Camacho <rasec.camacho19@gmail.com>
2023-08-21 21:37:26 -0700

    Routes

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { AppRouter } from './router/Approuter'
 
 export const JournalApp = () => {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <AppRouter />
     </>
   )
 }
