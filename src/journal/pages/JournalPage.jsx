adb287c6f4cd1d6b60369b49c579d60cd10cbe48
Rasec Camacho <rasec.camacho19@gmail.com>
2023-10-07 15:48:15 -0700

    Feature NothingSelected componenet

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,15 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView, SelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis praesentium blanditiis labore minima excepturi tempora deserunt minus ducimus quos similique, distinctio, fugit fugiat aut quae aspernatur quaerat ea dolores iusto.</Typography>
+      {/* <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis praesentium blanditiis labore minima excepturi tempora deserunt minus ducimus quos similique, distinctio, fugit fugiat aut quae aspernatur quaerat ea dolores iusto.</Typography> */}
+
+      <NothingSelectedView />
+      {/* <SelectedView /> */}
+
     </JournalLayout>
   )
 }
