2b7ad4119c4c448cf2beb41b969bbfbd248c4fd8
Rasec Camacho <rasec.camacho19@gmail.com>
2023-10-07 16:57:43 -0700

    Feature NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView, SelectedView } from "../views"
+import { NothingSelectedView, NoteView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis praesentium blanditiis labore minima excepturi tempora deserunt minus ducimus quos similique, distinctio, fugit fugiat aut quae aspernatur quaerat ea dolores iusto.</Typography> */}
 
-      <NothingSelectedView />
-      {/* <SelectedView /> */}
+      {/* <NothingSelectedView /> */}
+      <NoteView />
 
     </JournalLayout>
   )
